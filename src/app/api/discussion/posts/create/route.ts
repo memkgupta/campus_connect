a5a3822e95a3c50b8d9776630702c52f13336574
Mayank Gupta <mayankkumargupta03@gmail.com>
2024-12-18 23:15:28 +0530

    delete discussion

--- src/app/api/discussion/posts/create/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from "next/server";
-
-export const POST = async(req:NextRequest)=>{
-    const {title,content,attachment,author,channel,tags,parentPost} = await req.json()
-    
-}
